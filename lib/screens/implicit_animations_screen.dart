184f8284cb9310c697bd5464145a10196a64496d
seo <seo081225@gmail.com>
2023-10-02 17:44:57 +0900

    #1.3 Curves

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -29,6 +29,7 @@ class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             AnimatedContainer(
+              curve: Curves.elasticOut,
               duration: const Duration(seconds: 2),
               width: size.width * 0.8,
               height: size.width * 0.8,
@@ -36,7 +37,7 @@ class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
               transformAlignment: Alignment.center,
               decoration: BoxDecoration(
                 color: _visible ? Colors.red : Colors.amber,
-                borderRadius: BorderRadius.circular(_visible ? 300 : 0),
+                borderRadius: BorderRadius.circular(_visible ? 100 : 0),
               ),
             ),
             const SizedBox(
