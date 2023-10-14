e1161bb64bb8885083e71daddb47e361df3f4ea0
seo <seo081225@gmail.com>
2023-10-14 21:03:01 +0900

    #8.4 AnimateList

--- lib/screens/wallet_screen.dart
+++ lib/screens/wallet_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class WalletScreen extends StatefulWidget {
   const WalletScreen({super.key});
@@ -14,16 +15,24 @@ class _WalletScreenState extends State<WalletScreen> {
       appBar: AppBar(
         title: const Text('Wallet'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(20.0),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            CreditCard(
-              bgColor: Colors.purple,
-            ),
-            CreditCard(bgColor: Colors.black),
-            CreditCard(bgColor: Colors.blue),
-          ],
+            const CreditCard(bgColor: Colors.purple),
+            const CreditCard(bgColor: Colors.black),
+            const CreditCard(bgColor: Colors.blue),
+          ]
+              .animate(
+                interval: 500.milliseconds,
+              )
+              .fadeIn(
+                begin: 0,
+              )
+              .slideX(
+                begin: -1,
+                end: 0,
+              ),
         ),
       ),
     );
